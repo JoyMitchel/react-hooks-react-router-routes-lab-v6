e45a2927dcba055de3a23b35c84b7689cffdf7c9
joy.mitchel@student.moringaschool.com <JoyMitchel>
2025-04-26 23:18:31 +0300

    modified main.jsx

--- src/main.jsx
+++ src/main.jsx
@@ -1,7 +1,13 @@
-import "./index.css";
+import React from "react";
 import ReactDOM from "react-dom/client";
-import { createBrowserRouter, RouterProvider } from "react-router-dom";
-import routes from "./routes";
+import { RouterProvider, createBrowserRouter } from "react-router-dom";
+import { routeArray } from "./routes"; 
+
+const router = createBrowserRouter(routeArray);
 
 const root = ReactDOM.createRoot(document.getElementById("root"));
-root.render();
+root.render(
+  <React.StrictMode>
+    <RouterProvider router={router} />
+  </React.StrictMode>
+);
